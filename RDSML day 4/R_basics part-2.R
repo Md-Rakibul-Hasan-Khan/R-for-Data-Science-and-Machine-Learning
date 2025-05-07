0ed2a6d3f53bb419d512e1561798cfb1aee1ceb7
Md. Rakibul Hasan Khan <rakibulhasankhan09@gmail.com>
2025-05-07 22:50:37 +0600

    day 4 basics

---
+++ RDSML day 4/R_basics part-2.R
@@ -0,0 +1,63 @@
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = 'RAKIBUL HASAN'
+print(name)
+class(name)
+
+# what happens when sheathing a number into a bracket
+c = '53'
+print(c + 10)
+# non numeric argument is concluded
+
+print(a + 9)
+# numeric data type involved in math operations
+
+# logical data type - TRUE, FALSE (T, F)
+a > b
+class (a > b)
+print (a > b)
+
+# relational operation checking
+x = 19
+y = 22-3
+
+# equality function ==
+x == y
+print (x == y)
+# spaces in between didn't affect the outcome
+print(x==y)
+# not equal !=
+print(x!=y)
+print (y>=x)
+
+# Logical operators in R
+# && will be T if both side is true
+4==4 && 5==5
+class(4==4 && 5==5)
+
+# || will be T if any of the sides is true
+x != y || x == y
+
+# Simple mathematical operators
+z = 6 + 9 -8 * (2 / 3)
+print (z)
+
+w = 4 + 3 - 3 * 2 / 3
+print(w)
+v =  (4 + 3 - 3) * 2 / 3
+print (v)
+
+j = 4^2
+print(j)
+
+# remainder
+11/5
+11%%5
+11%/%5
+
+
+
+
